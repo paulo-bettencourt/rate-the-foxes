ba80bfb4dec3558b66bbb685021925b010df4873
paulolemos-overflow <paulo.lemos@bringglobal.com>
2023-01-19 12:02:25 -0100

    bugfix related to the voting system

--- src/app/presentation/presentation/presentation.component.ts
+++ src/app/presentation/presentation/presentation.component.ts
@@ -8,7 +8,7 @@ import {Fox} from "../../interfaces/fox.interface";
   templateUrl: './presentation.component.html',
   styleUrls: ['./presentation.component.scss']
 })
-export class PresentationComponent {
+export class PresentationComponent implements AfterViewInit{
 
   foxPicture: Fox = {
     image: '',
@@ -16,19 +16,32 @@ export class PresentationComponent {
   };
   coolCounter = 0;
   uncoolCounter = 0;
+  coolButton!: HTMLButtonElement;
+  uncoolButton!: HTMLButtonElement;
+  id!: any;
 
   constructor(private service: ApiService) {
     this.getNewFox();
   }
 
-  coolCounterButton() {
+  coolCounterButton(e: Event) {
     this.coolCounter++;
     this.getNewFox();
+    this.coolButton.disabled = true;
+    this.uncoolButton.disabled = true;
+    // @ts-ignore
+    this.id = e.target.id
+    document.body.style.cursor='wait';
   }
 
-  uncoolCounterButton() {
+  uncoolCounterButton(e: Event) {
     this.uncoolCounter++;
     this.getNewFox();
+    this.uncoolButton.disabled = true;
+    this.coolButton.disabled = true;
+    // @ts-ignore
+    this.id = e.target.id;
+    document.body.style.cursor='wait';
   }
 
   private getNewFox() {
@@ -39,9 +52,28 @@ export class PresentationComponent {
     )
   }
 
+  checkImageButton() {
+    this.id === 'cool-button' ? this.checkImageLoadedCoolButton() : this.checkImageLoadedUncoolButton();
+  }
 
+  checkImageLoadedCoolButton() {
+    this.coolButton.disabled = false
+    this.uncoolButton.disabled = false;
+    document.body.style.cursor='default';
+  }
 
+  checkImageLoadedUncoolButton() {
+    this.uncoolButton.disabled = false
+    this.coolButton.disabled = false;
+    document.body.style.cursor='default';
+  }
 
+  ngAfterViewInit(): void {
+    // @ts-ignore
+    this.coolButton = document.getElementById('cool-button');
+    // @ts-ignore
+    this.uncoolButton = document.getElementById('uncool-button');
+  }
 }
 
 
